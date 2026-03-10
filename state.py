d5f813b1c013f537bc0ac55ee130cb5b29a42da8
jeevankumar-m <jeevankumar06m@gmail.com>
2026-03-10 23:10:36 +0530

    state.py file created

---
+++ state.py
@@ -0,0 +1,8 @@
+#the states file only manages all the dictionaries of satellites, debris, cdm_warnings (conjunction data message warnings), sim_time (simulation time)
+
+satellites = {}
+debris = {}
+cdm_warnings = {}
+
+#seconds since the start of the simulation 
+sim_time = 0.0 
