302becc64d9599628781538b06fe456fbdc785c8
jeevankumar-m <jeevankumar06m@gmail.com>
2026-03-13 17:47:38 +0530

    added prediction point (yellow) and last burn point (purple point)

--- simulator.py
+++ simulator.py
@@ -15,9 +15,12 @@ ax.add_patch(earth)
 
 satellite, = ax.plot([], [], 'g^')
 debris, = ax.plot([], [], 'r^')
+collision_marker, = ax.plot([], [], 'yo', markersize=8)
 trail, = ax.plot([], [], 'green')
 debris_trail, = ax.plot([], [], 'red')
+burn_point, = ax.plot([], [], 'mo', markersize=6)
 
+maneuver_started = False
 
 state = np.array([6778, 0, 0, 0, 7.67, 0])
 debris_state = np.array([6780, 0, 0, 0, -7.66, 0])
@@ -33,9 +36,36 @@ dt = 20
 steps = 200
 max_trail = 200
 
+def predict_closest_approach(state, debris_state):
+    test_state = state.copy()
+    test_debris = debris_state.copy()
+
+    min_distance = 1e9 
+    collision_point = None 
+
+    for i in range(200):
+        test_state = rk4_step(test_state, dt)
+        test_debris = rk4_step(test_debris, dt)
+
+        dx = test_state[0] - test_debris[0]
+        dy = test_state[1] - test_debris[1]
+
+        distance = np.sqrt(dx * dx + dy * dy)
+        if distance < min_distance:
+            min_distance = distance
+            collision_point = (test_state[0], test_state[1])
+
+    return min_distance, collision_point
+
+
 while(True):
     state = rk4_step(state, dt)
     debris_state = rk4_step(debris_state, dt)
+
+    predicted_distance, collision_point = predict_closest_approach(state, debris_state)
+    
+    if collision_point != None:
+        collision_marker.set_data([collision_point[0]], [collision_point[1]])
 
     debris_x = debris_state[0]
     debris_y = debris_state[1]
@@ -48,9 +78,17 @@ while(True):
     distance = np.sqrt(dx * dx + dy * dy) 
     print(distance)
 
-    if distance < 100:
+    if predicted_distance < 50 and not maneuver_started:
         print("CDM Warning: Close Approach")
-
+        burn_point.set_data([x], [y])
+        v = state[3:6]
+        v_mag = np.linalg.norm(v)
+        v_unit = v / v_mag 
+        delta_v = 0.1
+        delta_v_vector = v_unit * delta_v 
+        state[3:6] = state[3:6] + delta_v_vector 
+        maneuver_started = True 
+ 
     x_list.append(x)
     y_list.append(y)
     x_list_debris.append(debris_x)
